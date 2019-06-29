ee458b7b9f5c995deaf0282c79c2f15cc1146a45
Brandon Rader <brandon.rader@gmail.com>
2019-06-28 22:33:25 -0500

    Updated footer and Instagram feed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,14 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://instagram.com/joyfullymade19/">@joyfullymade19</a>
     </h2>
     <br />
     <InstagramFeed count="8" />
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          © Copyright {new Date().getFullYear()} All rights reserved. Made, joyfully, by Brandon.
         </span>
       </div>
     </footer>
